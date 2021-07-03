2014e87e70fc62f5a2d4bb52dcffe60114556bba
xuan1106 <spu96508@gmail.com>
2021-07-03 21:29:23 +0800

    add search function

--- app.js
+++ app.js
@@ -10,12 +10,22 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', {restaurant: restaurantList.results})
+  res.render('index', { restaurant: restaurantList.results })
 })
 
-app.get('/restaurants/:restaurant_id',(req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', {restaurant:restaurant})
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => {
+    return restaurant.id.toString() === req.params.restaurant_id
+  })
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword)
+  })
+  res.render('index', { restaurant: restaurants, keyword: keyword })
 })
 app.listen(port, () => {
   console.log(`express server run in http://localhost:${port}`)
