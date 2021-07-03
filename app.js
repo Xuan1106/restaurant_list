41559927a85ec779e44aaed6cd913989822675b0
xuan1106 <spu96508@gmail.com>
2021-07-03 21:04:14 +0800

    set show page render data

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
   res.render('index', {restaurant: restaurantList.results})
 })
 
+app.get('/restaurants/:restaurant_id',(req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', {restaurant:restaurant})
+})
 app.listen(port, () => {
   console.log(`express server run in http://localhost:${port}`)
 })
