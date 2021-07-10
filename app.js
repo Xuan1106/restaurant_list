64e02e16f7c72faed47b5fa9b0fad7fe45345520
xuan1106 <spu96508@gmail.com>
2021-07-10 19:25:45 +0800

    add detail page

--- app.js
+++ app.js
@@ -50,12 +50,14 @@ app.post('/restaurants', (req, res) => {
     .catch(erroe => console.log(error))
 
 })
-// app.get('/restaurants/:restaurant_id', (req, res) => {
-//   const restaurant = restaurantList.results.find(restaurant => {
-//     return restaurant.id.toString() === req.params.restaurant_id
-//   })
-//   res.render('show', { restaurant: restaurant })
-// })
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
 
 // app.get('/search', (req, res) => {
 //   const keyword = req.query.keyword.toLowerCase()
