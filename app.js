2120fc24895a2c13a7a44f979cab67c18bfd345b
xuan1106 <spu96508@gmail.com>
2021-07-10 17:41:15 +0800

    modify index.handlebars

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const port = 3000
 const exphbs = require('express-handlebars')
 
 const restaurantList = require('./restaurant.json')
+const Restaurant = require('./models/restaurant.js')
 
 mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 
-db.on('error', ()=> console.log('mongodb error'))
+db.on('error', () => console.log('mongodb error'))
 db.once('open', () => console.log('mongodb connected!'))
 
 // set template engine
@@ -20,25 +21,29 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // set static file
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => {
-    return restaurant.id.toString() === req.params.restaurant_id
-  })
-  res.render('show', { restaurant: restaurant })
-})
+// app.get('/restaurants/:restaurant_id', (req, res) => {
+//   const restaurant = restaurantList.results.find(restaurant => {
+//     return restaurant.id.toString() === req.params.restaurant_id
+//   })
+//   res.render('show', { restaurant: restaurant })
+// })
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase()
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword)
-  })
-  res.render('index', { restaurant: restaurants, keyword: keyword })
-})
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword.toLowerCase()
+//   const restaurants = restaurantList.results.filter(restaurant => {
+//     return restaurant.name.toLowerCase().includes(keyword)
+//   })
+//   res.render('index', { restaurant: restaurants, keyword: keyword })
+// })
 app.listen(port, () => {
   console.log(`express server run in http://localhost:${port}`)
 })
