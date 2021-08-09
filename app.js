bcb9326ab42e7e26ee8b4a58114f0dfb7c4bb88c
xuan1106 <spu96508@gmail.com>
2021-08-09 19:06:54 +0800

    feat: add express-session and dotenv

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
+const PORT = process.env.PORT
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'process.env.SESSION_SECRET',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('express server run in http://localhost:3000')
 })
