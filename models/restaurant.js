43556112ac4c7f9f542e4fd356a68ed575095ec6
xuan1106 <spu96508@gmail.com>
2021-08-10 16:16:12 +0800

    feat: add relations User and Restaurant models

--- models/restaurant.js
+++ models/restaurant.js
@@ -38,6 +38,12 @@ const restaurantSchema = new Schema({
   description: {
     type: String,
     required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
   }
 })
 
